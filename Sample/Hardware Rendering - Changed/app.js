f9be0e19695477051d0fa667e6955cbb814a0802
buaasniper <shw316@lehigh.edu>
2017-05-17 13:55:53 -0700

    Add 3 samples

--- Sample/Hardware Rendering - Changed/app.js
+++ Sample/Hardware Rendering - Changed/app.js
@@ -20,7 +20,7 @@ var fragmentShaderText =
 'varying vec3 fragColor;',
 'void main()',
 '{',
-'	gl_FragColor = vec4(fragColor, 1.0);',
+'	gl_FragColor = vec4(floor(fragColor * 255.0)/255.0 , 1.0);',
 '}'
 ].join('\n');
 
@@ -74,9 +74,9 @@ var InitDemo = function(){
 	
 	var triangleVertices = 
 	[
-		0.0 , 0.5 , 1.0, 1.0, 0.0,
-		-0.5 , -0.5, 0.7, 0.0, 1.0,
-		0.5, -0.5, 0.1, 1.0, 0.6
+		1.0 , 1.0 , 1.0, 0.0, 0.0,
+		0.0 , 1.0, 0.0, 0.0, 1.0,
+		0.0, -0.0, 0.0, 0.0, 1.0
 	];
 	
 	var triangleVertextBufferObject = gl.createBuffer();
