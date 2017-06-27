fba58f43872fa0ff28bf9d93926a2efaf8a1d3f0
buaasniper <shw316@lehigh.edu>
2017-06-26 22:14:01 -0400

    add size test

--- test/server/CreateImage.py
+++ test/server/CreateImage.py
@@ -1,9 +1,9 @@
 from PIL import Image
-
-im = Image.new("RGB", (256, 256))
+n = 512
+im = Image.new("RGB", (n, n))
 pix = im.load()
-for x in range(256):
-    for y in range(256):
+for x in range(n):
+    for y in range(n):
         pix[x,y] = (34,30,31)
 im.save("testtest.png", "PNG")
 im.show()
