39054f3a197fa4244f00d70fd3a7bd2865493643
buaasniper <shw316@lehigh.edu>
2017-06-28 16:26:15 -0400

    add image 49

--- test/server/CreateImage.py
+++ test/server/CreateImage.py
@@ -1,10 +1,10 @@
 from PIL import Image
-n = 2
-im = Image.new("RGB", (n, n))
+n = 256
+im = Image.new("RGBA", (n, n))
 pix = im.load()
 for x in range(n):
     for y in range(n):
-        pix[x,y] = (34,30,31)
+        pix[x,y] = (x, x, x, y)
 im.save("testtest.png", "PNG")
 im.show()
 im = Image.open('testtest.png')
